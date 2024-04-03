848a7869cb7fa347256d92c8ac4ac0fe235a9299
William Faircloth <willuhmjs@gmail.com>
2024-04-03 16:20:42 -0400

    fix: remove redundant extension

--- src/routes/f/upload/+server.ts
+++ src/routes/f/upload/+server.ts
@@ -1,6 +1,5 @@
 import type { RequestHandler } from './$types';
 import { API_KEY } from '$env/static/private';
-import { extension } from "mime-types";
 import fs from "fs-extra";
 
 
@@ -9,7 +8,6 @@ export const POST: RequestHandler = async ({ request }) => {
     const api_key = formData.get('api_key');
     if (api_key !== API_KEY) return new Response("Unauthorized", { status: 401 });
     const file: File = (formData.get('file') as File);
-    const fileName = `${file.name}.${extension(file.type)}`
-    fs.outputFileSync(`./files/${fileName}`, Buffer.from(await file.arrayBuffer()));
-    return new Response(new URL(request.url).origin + '/f/' + fileName, { status: 201 })
+    fs.outputFileSync(`./files/${file.name}`, Buffer.from(await file.arrayBuffer()));
+    return new Response(new URL(request.url).origin + '/f/' + file.name, { status: 201 })
 };
