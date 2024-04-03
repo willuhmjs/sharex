8f8a6f24b391effc0cfa917249cba2e2c7503322
William Faircloth <willuhmjs@gmail.com>
2024-04-03 15:17:01 -0400

    fix: log error to console

--- src/routes/i/[id]/+server.ts
+++ src/routes/i/[id]/+server.ts
@@ -5,7 +5,8 @@ export const GET: RequestHandler = async ({ params }) => {
    try {
     const file = fs.readFileSync(`./files/${params.id}`);
     return new Response(file, { headers: { 'Content-Type': lookup(params.id) } });
-   } catch {
+   } catch (e) {
+   console.error(e);
     return new Response("Not Found", { status: 404 });
    }
 };
