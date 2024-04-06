31cea43bc1cd043b5f84507bf7fc5d98fe5a4341
William Faircloth <willuhmjs@gmail.com>
2024-04-06 15:45:58 -0400

    feat: make just file app

--- src/routes/upload/+server.ts
+++ src/routes/upload/+server.ts
@@ -2,12 +2,11 @@ import type { RequestHandler } from './$types';
 import { API_KEY } from '$env/static/private';
 import fs from "fs-extra";
 
-
 export const POST: RequestHandler = async ({ request }) => {
     const formData = await request.formData();
     const api_key = formData.get('api_key');
     if (api_key !== API_KEY) return new Response("Unauthorized", { status: 401 });
     const file: File = (formData.get('file') as File);
     fs.outputFileSync(`./files/${file.name}`, Buffer.from(await file.arrayBuffer()));
-    return new Response(new URL(request.url).origin + '/f/' + file.name, { status: 201 })
+    return new Response(new URL(request.url).origin + '/' + file.name, { status: 201 })
 };
