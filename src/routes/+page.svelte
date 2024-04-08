3ddc30b1edb7f6849e3b4ae6a05215d87131de60
willuhmjs <willuhmjs@gmail.com>
2024-04-07 21:27:41 -0400

    feat: file upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+let api_key = "";
+onMount(() => {
+  api_key = localStorage.getItem("api_key") || "";
+})
+
+</script>
+
+<form method="post" action="/upload" enctype="multipart/form-data" on:submit={() => localStorage.setItem("api_key", api_key)}>
+  <input required name="api_key" type="text" bind:value={api_key} placeholder="API Key"/>  
+  <input required name="file" type="file" />
+    <button>Upload</button>
+  </form>
